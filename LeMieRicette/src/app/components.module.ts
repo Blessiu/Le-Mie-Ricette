02359c28ea93988b53eac1cac4e840fc831c45c1
Davisim <38098547+Davisim@users.noreply.github.com>
2020-02-11 22:13:42 +0100

    fab button

--- LeMieRicette/src/app/components.module.ts
+++ LeMieRicette/src/app/components.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { FabComponent } from './fab/fab.component';
 
 @NgModule({
-  declarations:[ToolbarComponent],
+  declarations:[ToolbarComponent, FabComponent],
   imports:[IonicModule],
-  exports:[ToolbarComponent]
+  exports:[ToolbarComponent, FabComponent]
 })
 export class ComponentsModule{}
--- LeMieRicette/src/app/tab/frigorifero/frigorifero.module.ts
+++ LeMieRicette/src/app/tab/frigorifero/frigorifero.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { FrigoriferoPageRoutingModule } from './frigorifero-routing.module';
 
 import { FrigoriferoPage } from './frigorifero.page';
+import { ComponentsModule } from '../../components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsModule,
     FrigoriferoPageRoutingModule
   ],
   declarations: [FrigoriferoPage]
