46d3a6b336f6beb13c3280d54a87ac896427a39a
Davisim <38098547+Davisim@users.noreply.github.com>
2020-02-17 22:51:19 +0100

    timer

--- LeMieRicette/src/app/tab/timer/timer.page.ts
+++ LeMieRicette/src/app/tab/timer/timer.page.ts
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.page.scss'],
 })
 
+
+
 export class TimerPage implements OnInit {
 
   mydate:any;
   tempo:number;
   mm:number;
+  intervalVar:any;
+  alarmSound = new Audio();
   constructor() {
     this.mydate = 0;
     this.tempo = 0;
@@ -24,15 +28,45 @@ export class TimerPage implements OnInit {
   doSomething(item) {
     let d = new Date(item.detail.value);
     this.mm = d.getMinutes();
-    console.log(typeof this.mm);
     this.tempo = this.mm;
-    this.startTimer();
-  }
-  startTimer(){
-    this.tempo--;
-    console.log('start');
-    setTimeout(this.startTimer, 1000);
+    if(this.tempo != 0){
+      document.getElementById('alarmButton').style.display = '';
+    }
   }
 
+  start(){
+    document.getElementById('alarmStop').style.display = 'none';
+    document.getElementById('pausaAlarmBtn').style.display = '';
+    document.getElementById('alarmButton').style.display = 'none';
+    this.alarmSound.src = '../../assets/alarm.mp3';
+    this.intervalVar= setInterval(
+      function(){
+       if(this.tempo < 2){
+        this.tempo--;
+        this.alarmSound.play();
+        document.getElementById('alarmStop').style.display = '';
+        document.getElementById('pausaAlarmBtn').style.display = 'none';
+        console.log("tempo scaduto");
+        clearInterval(this.intervalVar);
+       } else{
+         this.tempo--;
+       }
+     }.bind(this),1000);
+  }
+
+  pausaAlarm() {
+    document.getElementById('alarmButton').style.display = '';
+    document.getElementById('pausaAlarmBtn').style.display = 'none';
+    document.getElementById('alarmStop').style.display = '';
+    clearInterval(this.intervalVar);
+  }
+
+  stop(){
+    this.mydate = 0;
+    this.tempo = 0;
+    this.alarmSound.pause();
+    document.getElementById('alarmStop').style.display = 'none';
+    document.getElementById('alarmButton').style.display = 'none';
+  }
   
 }
